8f8418cafee574a8b7778d340672cc55f9139192
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-09-02 08:28:44 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        const handler = (e)=>{
+            element.removeEventListener(eventName, handler);
+            const safeId = element.id.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${safeId}.`);
+        };
+        element.addEventListener(eventName, handler);
+    });
+}
+const printMessage = (message)=>{
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.textContent = message;
+    fragment.appendChild(div);
+    document.body.appendChild(fragment);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
